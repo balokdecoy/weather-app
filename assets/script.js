79d8c2d8c2689d9343061a261615666392ef993f
johnniesimpson <johnniesimpson@outlook.com>
2020-12-18 16:17:39 -0500

    Added jumbotron header and adjusted styling. Built queryURL for weather api call.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-// $.ajax({
-//     url: "api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key},
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response);
-//   });
+var apiKey = '31935cf21b5d358b8ba4c4da65b29118';
+var cityName = 'baltimore';
+var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
 
-
+$.ajax({
+    url: queryURL,
+    method: 'GET',
+})
