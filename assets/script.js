45a7f921fb0df00cd9c482d3e328e9942b21b391
johnniesimpson <johnniesimpson@outlook.com>
2020-12-18 17:22:24 -0500

    Building HTML frame, added some styling. Touched up API call.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 var apiKey = '31935cf21b5d358b8ba4c4da65b29118';
-var cityName = 'baltimore';
+var cityName = '';
 var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
 
 $.ajax({
     url: queryURL,
     method: 'GET',
-})
+}).then(function (response) { 
+    console.log(response);
+ })
