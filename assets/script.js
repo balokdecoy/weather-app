0c3381580ddb8badbfff0dc8c8bfd11d9a7d8458
johnniesimpson <johnniesimpson@outlook.com>
2020-12-17 22:44:29 -0500

    Pseudo-coding GET API.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// $.ajax({
+//     url: "api.openweathermap.org/data/2.5/forecast?q={city name},{state code}&appid={API key},
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//   });
+
+
