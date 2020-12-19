9c69ef95e5f1fbf2f17dcac147d340c08b2d6822
johnniesimpson <johnniesimpson@outlook.com>
2020-12-18 19:01:59 -0500

    Refactored JS to nest ajax call in submit function. Added console log to test.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,17 @@
-var apiKey = '31935cf21b5d358b8ba4c4da65b29118';
-var cityName = '';
-var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
 
-$.ajax({
-    url: queryURL,
-    method: 'GET',
-}).then(function (response) { 
-    console.log(response);
- })
 
  $(document).ready(function () {
     $('#submit').click(function (e) { 
         e.preventDefault();
-        console.log($('#citySearch').val());
+        var cityName = $('#citySearch').val();
+        var apiKey = '31935cf21b5d358b8ba4c4da65b29118';
+        var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+        $.ajax({
+            url: queryURL,
+            method: 'GET',
+        }).then(function (response) { 
+            console.log(response);
+         })
     });
  });
  
