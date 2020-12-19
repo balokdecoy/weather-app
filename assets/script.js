a970439ca1030dfad8b166aa5c1c932756689cc4
johnniesimpson <johnniesimpson@outlook.com>
2020-12-18 21:20:16 -0500

    Continuing to add data to forecast card.

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@
         var cityName = $('#citySearch').val();
         console.log(cityName);
         var apiKey = '31935cf21b5d358b8ba4c4da65b29118';
-        var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+        var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
         $.ajax({
             url: queryURL,
             method: 'GET',
@@ -14,10 +14,21 @@
         }).then(function (response) { 
             clearContents();
             console.log(response);
+            // Create div where current conditions will render
             var cityWeather = $("<div class=cityWeather>");
+            // Location
             var current = $('<p>').text(response.name);
+            // Temperature
+            var temp = $('<p>').text('Temp: ' + response.main.temp + ' \u2109');
+            // Weather conditions
             var currWeather = $('<p>').text('Currently: ' + response.weather[0].main);
-            cityWeather.append(current, currWeather);
+            // Humidity
+            var humidity = $('<p>').text('Humidity: ' + response.main.humidity + "%");
+            // Wind Speed
+            var windSpeed = $('<p>').text('Wind Speed: ' + response.wind.speed + ' mph');
+            // Add data elements to div
+            cityWeather.append(current, currWeather, temp, humidity, windSpeed);
+            // Add div to html
             $('#todayForecast').append(cityWeather);
          })
     });
