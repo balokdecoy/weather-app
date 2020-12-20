0edec389eeefad040d6dec8b8c273cb686350d79
johnniesimpson <johnniesimpson@outlook.com>
2020-12-20 13:26:44 -0500

    Refactored functions.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,27 @@
 // Set date
 var currentDate = dayjs().format('MM/DD/YYYY');
 
- $(document).ready(function () {
+// Set API key 
+var apiKey = '31935cf21b5d358b8ba4c4da65b29118';
+
+// Div where current conditions will render
+var cityWeather = $("<div class=cityWeather>");
+
+$(document).ready(function () {
+    var pastCity = localStorage.getItem('city');
+    console.log(pastCity);
+    getLocalWeather(pastCity);
     $('#submit').click(function (e) { 
         e.preventDefault();
         $('.card').attr('style', 'visibility: visible');
-        
         var cityName = $('#citySearch').val();
-        var apiKey = '31935cf21b5d358b8ba4c4da65b29118';
+        getLocalWeather(cityName);
+    });
+
+    function getLocalWeather(cityName) {
+        clearContents();
         var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
+        
         // Call local weather data
         $.ajax({
             url: queryURL,
@@ -19,8 +32,6 @@ var currentDate = dayjs().format('MM/DD/YYYY');
             console.log(response);
 
             // SET VARIABLES
-            // Div where current conditions will render
-            var cityWeather = $("<div class=cityWeather>");
             // Weather image
             var imgWeather = $('<img src="https://openweathermap.org/img/wn/' + response.weather[0].icon + '@2x.png" alt="weather icon">');
             // Location
@@ -39,48 +50,77 @@ var currentDate = dayjs().format('MM/DD/YYYY');
             var lat = response.coord.lat;
             // UV Index URL
             var uvQuery = 'https://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + long + '&appid=' + apiKey;
-           
-            // Call UV Index API
-            $.ajax({
-                url: uvQuery,
-                method: 'GET',
-                datatType: 'jsonp',
-            }).then(function(data) {
-                console.log(data);
-                var currentUV = $('<p>UV Index: </p>');
-                var uvIndex = $('<span>' + data.value + '</span>');
-                $(currentUV).append(uvIndex);
-                // Assign UV color coding class based on EPA guidelines
-                if (data.value < 3) {
-                    $(uvIndex).addClass('low');
-                }
-                else if (data.value >= 3 && data.value < 6 ) {
-                    $(uvIndex).addClass('moderate');
-                }
-                else if (data.value >= 6 && data.value < 8) {
-                    $(uvIndex).addClass('high');
-                }
-                else if (data.value >= 8 && data.value < 11) {
-                    $(uvIndex).addClass('veryHigh');
-                }
-                else {
-                    $(uvIndex).addClass('extreme');
-                }
-                // Send UV info to display div
-                cityWeather.append(currentUV);
-            })
-
-            var addHistory = $('<ul><<button type="button" class="btn btn-primary">' + cityName + '</button></ul>');
-            $('.history').append(addHistory);
-
             // Add data elements to div
             cityWeather.append(current, imgWeather, currWeather, temp, humidity, windSpeed,);
             // Add div to html
             $('#todayForecast').append(cityWeather);
-         })
-    });
- });
 
- function clearContents() {
-     $('#todayForecast').text('');
- }
+            // Set local storage
+            localStorage.setItem('city', cityName);
+
+            // Trigger UV function
+            getUV(long, lat);
+
+            // Trigger 5 day forecast
+            getFive(long, lat);
+        })
+    }
+
+    function getUV(long, lat) {
+        // UV Index URL
+        var uvQuery = 'https://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + long + '&appid=' + apiKey;
+    
+        // Call UV Index API
+        $.ajax({
+            url: uvQuery,
+            method: 'GET',
+        }).then(function(data) {
+            console.log(data);
+            var currentUV = $('<p>UV Index: </p>');
+            var uvIndex = $('<span>' + data.value + '</span>');
+            $(currentUV).append(uvIndex);
+            // Assign UV color coding class based on EPA guidelines
+            if (data.value < 3) {
+                $(uvIndex).addClass('low');
+            }
+            else if (data.value >= 3 && data.value < 6 ) {
+                $(uvIndex).addClass('moderate');
+            }
+            else if (data.value >= 6 && data.value < 8) {
+                $(uvIndex).addClass('high');
+            }
+            else if (data.value >= 8 && data.value < 11) {
+                $(uvIndex).addClass('veryHigh');
+            }
+            else {
+                $(uvIndex).addClass('extreme');
+            }
+            // Send UV info to display div
+            cityWeather.append(currentUV);
+        })
+    }
+
+    function getFive(long, lat) {
+        var fiveQuery = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&appid=' + apiKey;
+        $.ajax({
+            url: fiveQuery,
+            method: 'GET',
+        }).then(function(fiveData) {
+            console.log(fiveData);
+
+        })
+    }
+
+    function getPast() {
+
+    }
+
+    function clearContents() {
+        $('#todayForecast').text('');
+    }
+})
+
+ // Event.target.text.val(); 
+ // Could set value equal to city name
+ // getLocalData();
+ // getUvIndex();
