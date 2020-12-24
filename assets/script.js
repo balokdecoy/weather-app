b24c35f12e0016e8877f75eda1c59f0820541887
johnniesimpson <johnniesimpson@outlook.com>
2020-12-23 19:36:00 -0500

    Added cursor pointer to search history list items.

--- assets/script.js
+++ assets/script.js
@@ -122,7 +122,8 @@ $(document).ready(function () {
             console.log(fiveData);
             for (var i = 1; i < 6; i++) {
                 // Set five day forecast variables
-                var dayDiv = $('<div class=col-2></div>')
+                var dayDiv = $('<div class="col-2 card bg-info mb-3"></div>')
+                $(dayDiv).attr('style', 'color: white; margin: auto;');
                 var day = dayjs().date();
                 var adjDay = (day + i);
                 var month = dayjs().format('MM');
